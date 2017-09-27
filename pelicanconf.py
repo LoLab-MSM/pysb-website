2260834ef2df387372c6521dea564caafbffe22c
Alex Lubbock <code@alexlubbock.com>
2017-09-27 16:36:35 -0500

    Remove .html from end of URLs

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,14 +23,21 @@ for f in os.listdir('content/extra/'):
     EXTRA_PATH_METADATA['extra/'+f] = {'path':f}
 
 MENUITEMS=[('Home', '/'),
-           ('Download', '/download.html'),
-           ('News', '/category/news.html'),
-           ('Tutorials', '/category/tutorials.html'),
-           ('Support', '/support.html')]
+           ('Download', '/download'),
+           ('News', '/news'),
+           ('Tutorials', '/tutorials'),
+           ('Support', '/support')]
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
-PAGE_URL = '{slug}.html'
+PAGE_URL = '{slug}'
 PAGE_SAVE_AS = '{slug}.html'
+ARTICLE_URL = '{category}/{slug}'
+ARTICLE_SAVE_AS = '{category}/{slug}.html'
+CATEGORY_URL = '{slug}/'
+CATEGORY_SAVE_AS = '{slug}/index.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}.html'
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
